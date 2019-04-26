c68698afad6cac403682221053806a94d3595aa2
Renan Alves <alvees.renan@gmail.com>
2019-04-25 23:24:24 -0300

    Merge with master; Removing rabbitmq dependency form project.

--- src/main/java/br/com/alveesrenan/demos/kuberneteshpacustom/etl/amqp/configs/ProcessorConfiguration.java
+++ src/main/java/br/com/alveesrenan/demos/kuberneteshpacustom/etl/amqp/configs/ProcessorConfiguration.java
@@ -1,16 +1,8 @@
 package br.com.alveesrenan.demos.kuberneteshpacustom.etl.amqp.configs;
 
 import br.com.alveesrenan.demos.kuberneteshpacustom.etl.amqp.configs.props.ProcessorQueueProperties;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.HashMap;
-import java.util.concurrent.ConcurrentHashMap;
 
 @Configuration
 public class ProcessorConfiguration {
@@ -22,20 +14,20 @@ public class ProcessorConfiguration {
         this.props = props;
     }
 
-    @Bean
-    public TopicExchange exchange() {
-        return new TopicExchange(props.getExchange());
-    }
-
-    @Bean
-    public Queue queue() {
-        return new Queue(props.getName(), true, false, false, new ConcurrentHashMap<>());
-    }
-
-    @Bean
-    public Binding bindingExchangeToQueueByRoutingKey() {
-        return BindingBuilder.bind(queue())
-                .to(exchange())
-                .with(props.getRoutingKey());
-    }
+//    @Bean
+//    public TopicExchange exchange() {
+//        return new TopicExchange(props.getExchange());
+//    }
+//
+//    @Bean
+//    public Queue queue() {
+//        return new Queue(props.getName(), true, false, false, new ConcurrentHashMap<>());
+//    }
+//
+//    @Bean
+//    public Binding bindingExchangeToQueueByRoutingKey() {
+//        return BindingBuilder.bind(queue())
+//                .to(exchange())
+//                .with(props.getRoutingKey());
+//    }
 }
