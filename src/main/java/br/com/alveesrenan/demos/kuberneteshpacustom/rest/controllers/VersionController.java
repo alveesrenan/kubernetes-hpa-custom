77b4fccaf8317b1eef2caa2e41a59d80fbb23c27
Renan Alves <ralves@atech.com.br>
2019-05-02 16:57:52 -0300

    Added elastic beanstalk artifacts to project.

--- src/main/java/br/com/alveesrenan/demos/kuberneteshpacustom/rest/controllers/VersionController.java
+++ src/main/java/br/com/alveesrenan/demos/kuberneteshpacustom/rest/controllers/VersionController.java
@@ -8,6 +8,6 @@ public class VersionController {
 
     @GetMapping("/version")
     public String currentVersion() {
-        return "Version 6 from project.";
+        return "Version 7 from project.";
     }
 }
