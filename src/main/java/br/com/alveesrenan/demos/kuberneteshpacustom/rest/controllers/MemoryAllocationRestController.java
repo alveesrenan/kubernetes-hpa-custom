4a892aff6c4d320ae2e7b26602f2212d5fd4ed9f
Renan Alves <alvees.renan@gmail.com>
2019-09-11 15:48:03 -0300

    Fixing api version for swagger;

--- src/main/java/br/com/alveesrenan/demos/kuberneteshpacustom/rest/controllers/MemoryAllocationRestController.java
+++ src/main/java/br/com/alveesrenan/demos/kuberneteshpacustom/rest/controllers/MemoryAllocationRestController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/api")
 public class MemoryAllocationRestController {
 
     @Autowired
--- src/main/java/br/com/alveesrenan/demos/kuberneteshpacustom/rest/controllers/VersionController.java
+++ src/main/java/br/com/alveesrenan/demos/kuberneteshpacustom/rest/controllers/VersionController.java
@@ -1,13 +1,15 @@
 package br.com.alveesrenan.demos.kuberneteshpacustom.rest.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1")
 public class VersionController {
 
     @GetMapping("/version")
     public String currentVersion() {
-        return "Version 7 from project.";
+        return "Version 1 from project.";
     }
 }
