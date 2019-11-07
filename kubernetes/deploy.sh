19710486344dcf3a7abaa0e9f97403491dbf71d4
Renan Alves <ralves@atech.com.br>
2019-11-07 16:28:34 -0200

    Fixing deploy.sh with helm upgrade command.

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -9,15 +9,9 @@ check_status() {
 
 CHART_NAME=kubernetes-hpa-custom
 CHART_VERSION=1.0.0
-CHART_NAMESPACE=eap-dev
+CHART_NAMESPACE=default
 HELM_EXTRA_CMD='--atomic --recreate-pods'
-PROXY=
 
-if [[ $1 == 'emb' ]]; then
-  PROXY=http://$2:$3@lnx237in.sjk.emb:9090
-fi
-
-docker run --rm -e "HELM_CHART=${CHART_NAME}" -e "CHART_NAMESPACE=${CHART_NAMESPACE}" -e "HELM_CHART_VERSION=${CHART_VERSION}" \
-  -e "HELM_EXTRA_CMD=${HELM_EXTRA_CMD}" -e "https_proxy=${PROXY}" -e "http_proxy=${PROXY}" \
-  503622861177.dkr.ecr.us-east-1.amazonaws.com/kubectl-helm-eap:2.14.3-aws sh deploy-cmd.sh
+helm upgrade --install $CHART_NAME ./$CHART_NAME --version $CHART_VERSION --namespace $CHART_NAMESPACE \
+    --recreate-pods --atomic --cleanup-on-fail
 check_status $? 'Error when upgrading/installig helm chart.'
